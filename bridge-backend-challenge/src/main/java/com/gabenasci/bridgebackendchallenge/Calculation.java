9710a198cfa01a41c4ff6ed8e35394464f8f853d
gabenasci <gabenascimento1@gmail.com>
2022-08-08 19:49:40 -0300

    Otimização do cálculo

--- bridge-backend-challenge/src/main/java/com/gabenasci/bridgebackendchallenge/Calculation.java
+++ bridge-backend-challenge/src/main/java/com/gabenasci/bridgebackendchallenge/Calculation.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map.Entry;
+import java.math.*;
 
 public class Calculation {
 	
@@ -11,39 +13,55 @@ public class Calculation {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Insert number (k): ");
 		int k = sc.nextInt();
-		System.out.println(calculateNAndDivisors(k));
-		System.out.println(calculateResult(calculateNAndDivisors(k)));
+		long startTime = System.currentTimeMillis();
+		HashMap<Integer, Integer> nDivisors = calculateNAndDivisors(k);
+		System.out.println(nDivisors);
+		System.out.println(calculateResult(nDivisors));
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime+"ms Total");
 	}
-	public static int calculateResult(HashMap<Integer, ArrayList<Integer>> nDivisors) {
+	public static int calculateResult(HashMap<Integer, Integer> nDivisors) {
+		long startTime = System.currentTimeMillis();
 		int result = 0;
 		// Inicializar varíavel ArrayList dos divisores n-1
-		ArrayList<Integer> prevDivisors = new ArrayList<>(Arrays.asList(0));
+//		ArrayList<Integer> prevDivisors = new ArrayList<>(Arrays.asList(0));
 		
 		// Iterar o HashMap buscando o valor (ArrayList de Divisores) de cada chave (n)
-		for(ArrayList<Integer> divisors: nDivisors.values()) {
-			if(prevDivisors.size() == divisors.size()) {
+		for(int i = 2; i < nDivisors.size()+2; i++) {
+			if(nDivisors.get(i) == nDivisors.get(i+1)) {
 				result++; // Acrescentar contador se n-1 possui mesmo número de divisores de n
+//				System.out.println("partial result: "+result);
 			}
-			prevDivisors = divisors;
 		}
+
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime+"ms to Calculate Result (compare all n divisors)");
 		return result;
 	}
-	public static HashMap<Integer, ArrayList<Integer>> calculateNAndDivisors(int k) {
-		// HashMap que contém os inteiros positivos n menores que k e seus respectivos divisores
-		HashMap<Integer, ArrayList<Integer>> nDivisors = new HashMap<>();
+	public static HashMap<Integer, Integer> calculateNAndDivisors(int k) {
+		// HashMap que contém os inteiros positivos n menores que k e seus respectivos números de divisores
+		long startTime = System.currentTimeMillis();
+		HashMap<Integer, Integer> nDivisors = new HashMap<>();
 		for(int n = 2; n < k; n++) {
 			nDivisors.put(n, calculateDivisors(n));
 		}
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime+"ms to populate HashMap");
 		return nDivisors;
 	}
 	
-	public static ArrayList<Integer> calculateDivisors(int n) {
-		ArrayList<Integer> divisorsOfN = new ArrayList<>();
-		for(int i = 1; i<=n; i++) {
+	public static int calculateDivisors(int n) {
+		int count = 0;
+		for(int i = 1; i <= Math.sqrt(n); i++) {
 			if(n % i == 0) {
-				divisorsOfN.add(i);
+				if (n / i == i) {
+					count++;					
+				}
+				else 
+					count = count + 2;
 			}
 		}
-		return divisorsOfN;
+//		System.out.println("count: "+count);
+		return count;
 	}
 }
